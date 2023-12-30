369a317b63b27bd0d99d832eb9326942d2fe169c
Kshitij Gupta <guptakshitij2308@gmail.com>
2023-12-30 22:48:03 +0530

    Made reusable react components using styled-components library

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
+import { styled } from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import React from "react"; // eslint-disable-line
+import Heading from "./ui/Heading.jsx";
+import Row from "./ui/Row.jsx";
+
+const StyledApp = styled.div`
+  padding: 20px;
+`;
+
 function App() {
-  return <div>The Continental</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        {/* <Heading type="h1">The Continental</Heading> */}
+        <Row>
+          {/* type of row vertical set to vertical by default so no need to define it explictly */}
+          <Row type="horizontal">
+            <Heading as="h1">The Continental</Heading>
+            <div>
+              <Heading as="h2">Check in and Check out</Heading>
+              <Button>Check In</Button>
+              <Button variation="secondary" size="small">
+                Check Out
+              </Button>
+            </div>
+          </Row>
+
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
---
+++ src/ui/Heading.jsx
@@ -0,0 +1,34 @@
+import { styled, css } from "styled-components"; // eslint-disable-line
+
+// const test = css`
+//   text-align: center;
+// `;
+
+/*  ${test} */
+
+const Heading = styled.h1`
+  ${(props) =>
+    props.as === "h1" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+    `}
+
+  ${(props) =>
+    // props.type === "h2" &&
+    props.as === "h2" &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
+    `}
+
+  ${(props) =>
+    props.type === "h3" &&
+    css`
+      font-size: 2rem;
+      font-weight: 500;
+    `}
+    line-height: 1.4;
+`;
+
+export default Heading;
