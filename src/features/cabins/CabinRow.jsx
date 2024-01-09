64caf8b8e93de902816899338836c1840aa40828
Kshitij Gupta <guptakshitij2308@gmail.com>
2024-01-09 13:15:13 +0530

    Added updation of settings using react query.

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -4,6 +4,8 @@ import { formatCurrency } from "../../utils/helpers.js";
 import { useState } from "react";
 import CreateCabinForm from "./CreateCabinForm.jsx";
 import { useDeleteCabin } from "./DeleteCabin.js";
+import { HiSquare2Stack, HiTrash, HiPencil } from "react-icons/hi2";
+import { useCreateCabin } from "./useCreateCabin.js";
 
 const TableRow = styled.div`
   display: grid;
@@ -53,9 +55,22 @@ const CabinRow = ({ cabin }) => {
     maxCapacity,
     regularPrice,
     discount,
+    description,
   } = cabin;
 
   const { isDeleting, deleteCabin } = useDeleteCabin();
+  const { isCreating, createCabin } = useCreateCabin();
+
+  function handleDuplicateCabin() {
+    createCabin({
+      name: `Copy of ${name}`,
+      image,
+      maxCapacity,
+      regularPrice,
+      discount,
+      description,
+    });
+  }
 
   return (
     <>
@@ -70,9 +85,14 @@ const CabinRow = ({ cabin }) => {
           <span>&mdash;</span>
         )}
         <div>
-          <button onClick={() => setShowForm((show) => !show)}>Edit</button>
+          <button onClick={() => handleDuplicateCabin()} disabled={isCreating}>
+            <HiSquare2Stack />
+          </button>
+          <button onClick={() => setShowForm((show) => !show)}>
+            <HiPencil />
+          </button>
           <button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}>
-            Delete
+            <HiTrash />
           </button>
         </div>
       </TableRow>
--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,69 @@
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import { useSettings } from "./useSettings.js";
+import Spinner from "./../../ui/Spinner";
+import { useUpdateSetting } from "./useUpdateSetting";
 
 function UpdateSettingsForm() {
+  const { settings, isLoading } = useSettings();
+  const {
+    minBookingLength,
+    maxBookingLength,
+    maxGuestsPerBooking,
+    breakfastPrice,
+  } = settings || {};
+
+  const { updateSetting, isUpdating } = useUpdateSetting();
+
+  if (isLoading) return <Spinner />;
+
+  function handleUpdate(e, field) {
+    if (!e.target.value) return;
+    if (e.target.value == settings[field]) return;
+    // console.log(e.target.value, settings[field]);
+
+    updateSetting({ [field]: e.target.value });
+  }
+
   return (
     <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+      <FormRow label="Minimum nights/booking">
+        <Input
+          type="number"
+          id="min-nights"
+          defaultValue={minBookingLength}
+          onBlur={(e) => handleUpdate(e, "minBookingLength")}
+          disabled={isUpdating}
+        />
+        {/* using on blur we can update a setting as soon as we leave the field and our focus changes. */}
       </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+      <FormRow label="Maximum nights/booking">
+        <Input
+          type="number"
+          id="max-nights"
+          defaultValue={maxBookingLength}
+          onBlur={(e) => handleUpdate(e, "maxBookingLength")}
+          disabled={isUpdating}
+        />
       </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+      <FormRow label="Maximum guests/booking">
+        <Input
+          type="number"
+          id="max-guests"
+          defaultValue={maxGuestsPerBooking}
+          onBlur={(e) => handleUpdate(e, "maxGuestsPerBooking")}
+          disabled={isUpdating}
+        />
       </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+      <FormRow label="Breakfast price">
+        <Input
+          type="number"
+          id="breakfast-price"
+          defaultValue={breakfastPrice}
+          onBlur={(e) => handleUpdate(e, "breakfastPrice")}
+          disabled={isUpdating}
+        />
       </FormRow>
     </Form>
   );
